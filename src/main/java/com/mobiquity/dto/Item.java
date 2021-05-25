c062e19f8de68789b9acb74f6f885e5160e92240
Matheus Pusinhol <matheus.pusinhol.ext@dasa.com.br>
2021-05-25 15:27:58 -0300

    Update Item DTO

--- src/main/java/com/mobiquity/dto/Item.java
+++ src/main/java/com/mobiquity/dto/Item.java
@@ -1,2 +1,19 @@
-package com.mobiquity.dto;public class Item {
+package com.mobiquity.dto;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Item implements Serializable {
+
+    @EqualsAndHashCode.Include
+    private Integer id;
+    private Double weight;
+    private Double value;
 }
